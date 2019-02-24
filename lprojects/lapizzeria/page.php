fe3370caf2b616891287de48adeaec5d5df2360c
viseth89 <viseth89@gmail.com>
2019-02-24 12:13:44 -0600

    background styled and compared with previous bootstrap style implementation

--- lprojects/lapizzeria/page.php
+++ lprojects/lapizzeria/page.php
@@ -4,12 +4,25 @@ get_header();
 <!-- This is the template that is used to display 'Pages' in the Theme -->
 
 <?php while (have_posts()): the_post(); ?>
+<!-- Inline style only used for example and as Reference!!!! -->
+<div class="hero" style='background-image:url(<?php echo get_the_post_thumbnail_url(); ?>)  ;'>
+    <div class="hero-content">
+        <div class="hero-text">
+            <h2>
+                <?php the_title(); ?>
+            </h2>
+        </div>
+    </div>
+</div>
 
-<?php the_post_thumbnail(); ?>
-<h2>
-    <?php the_title(); ?>
-</h2>
-<?php the_content(); ?>
+<div class="main-content container">
+    <main class="text-center content-text">
+        <?php the_content(); ?>
+    </main>
+</div>
+
+
+
 
 <?php endwhile; ?>
 
