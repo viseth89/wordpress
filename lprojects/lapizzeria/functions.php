5b81c58b5b4b30cbff3d7ec0941582be52d2617f
viseth89 <viseth89@gmail.com>
2019-03-06 15:36:15 -0600

    added social links

--- lprojects/lapizzeria/functions.php
+++ lprojects/lapizzeria/functions.php
@@ -4,12 +4,14 @@ function lapizzeria_styles(){
   // Adding stylesheets
 
   wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0' );
+  wp_register_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.css', array(), '4.7' );
   wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0' );
   // passed in dependency for style/normalize
 
   // Enqueue the style
 
   wp_enqueue_style('normalize');
+  wp_enqueue_style('fontawesome');
   wp_enqueue_style('style');
 
 }
--- lprojects/lapizzeria/header.php
+++ lprojects/lapizzeria/header.php
@@ -12,36 +12,35 @@
 <body>
 
     <header class="site-header">
-        <div class="logo">
-            <!-- Escl used, makes web-site more secure -->
-            <a href=" <?php echo esc_url(home_url('/')) ?> ">
-                <img src="<?php echo get_template_directory_uri() ?>/img/logo.svg " class='logoimage' alt="">
-            </a>
-            <h1> <?php echo get_template_directory_uri() ?> </h1>
-        </div> <!-- End of logo -->
-        <div class="header-information">
-            <div class="socials">
+        <div class="container">
+            <div class="logo">
+                <!-- Escl used, makes web-site more secure -->
+                <a href=" <?php echo esc_url(home_url('/')) ?> ">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/logo.svg " class='logoimage' alt="">
+                </a>
+            </div> <!-- End of logo -->
+            <div class="header-information">
+                <div class="socials">
 
-            <?php
-            $args = array(
-              'theme_location'  =>  'social-menu',
-              'container'       =>  'nav',
-              'container_class' =>  'socials',
-              'container_id'    =>  'socials',
-              'link_before'     =>  '<span class="sr-text">',
-              'link_after'      =>  '</span>'
-            );
-            wp_nav_menu($args);
-            ?>
+                    <?php
+                    $args = array(
+                      'theme_location'  =>  'social-menu',
+                      'container'       =>  'nav',
+                      'container_class' =>  'socials',
+                      'container_id'    =>  'socials',
+                      'link_before'     =>  '<span class="sr-text">',
+                      'link_after'      =>  '</span>'
+                    );
+                    wp_nav_menu($args);
+                    ?>
 
-            </div> <!-- end of Socials -->
-            <div class="address">
-              <p>8179 Bay Avenue Moutain View, CA 94043</p>
-              <p>Phone Number: +1-92-456-7890</p>
-            </div>
-        </div>
-
-
+                </div> <!-- end of Socials -->
+                <div class="address">
+                    <p>8179 Bay Avenue Moutain View, CA 94043</p>
+                    <p>Phone Number: +1-92-456-7890</p>
+                </div>
+            </div> <!-- end of header-information -->
+        </div> <!-- end of container -->
     </header>
 
     <div class="main-menu">
