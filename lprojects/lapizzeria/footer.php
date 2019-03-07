334a40f6780a5dd94d28f84a001d6d5a3a3158f6
viseth89 <viseth89@gmail.com>
2019-03-07 15:22:16 -0600

    finished about us page, used '&' alot

--- lprojects/lapizzeria/footer.php
+++ lprojects/lapizzeria/footer.php
@@ -1,3 +1,24 @@
-  <?php wp_footer(); ?>
+
+<footer>
+  <?php
+    $args = array(
+      'theme_location'  =>  'header-menu',
+      'container'       =>  'nav',
+      'after'           => '<span class="separator"> | </span>'
+    );
+    wp_nav_menu($args);
+
+?>
+
+<div class="location">
+  <p>8179 Bay Avenue  Mountain View, CA 94043</p>
+  <p>Phone Number: +1-92-456-7890</p>
+</div>
+
+<p class="copyright">All rights reserved <?php echo date('Y'); ?> </p>
+</footer>
+
+
+<?php wp_footer(); ?>
 </body>
 </html>
