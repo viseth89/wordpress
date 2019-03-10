fedcd39063423bbc675c398fedd298b90ff2d104
viseth89 <viseth89@gmail.com>
2019-03-10 11:50:07 -0500

    finished blog, working on gallery

--- lprojects/lapizzeria/single.php
+++ lprojects/lapizzeria/single.php
@@ -1,10 +1,61 @@
-<!-- THis is where the template for posts -->
-<?php 
-get_header();
-?>
+<!-- This is the template for single blog posts -->
+<?php get_header(); ?>
 
-Hello from single.php 
+<!-- similar to sql = "select * from "  -->
+<!-- similar to django while/for loop -->
+<?php while (have_posts()) : the_post(); ?>
 
-<?php 
-get_footer();
-?>
+<div class="hero" style="background-image:url(<?php echo get_the_post_thumbnail_url(); ?>);">
+    <div class="hero-content">
+        <div class="hero-text">
+            <h2><?php the_title(); ?></h2>
+        </div>
+    </div>
+</div>
+
+<div class="main-content container">
+    <main class="text-center content-text">
+
+        <div class="entry-information clear">
+            <div class="date">
+                <time>
+                    <?php echo the_time('d'); ?>
+                    <span> <?php echo the_time('M'); ?> </span>
+                </time>
+            </div> <!-- .date -->
+
+            <p class="author">
+                <i class="fa fa-user" aria-hidden="true">
+                    <?php the_author(); ?>
+                </i>
+            </p><!-- .author -->
+        </div>
+
+        <p><?php the_content(); ?> </p>
+    </main>
+</div>
+
+<div class="container comments">
+  <?php comment_form(); ?>
+</div>
+
+<div class="container comment-list">
+  <ol class="commentlist">
+    <?php
+      $comments = get_comments(array(
+        'post_id' =>  $post->ID,
+        'status'  =>  'approve' 
+
+      ));
+      wp_list_comments(array(
+        'per_page'  =>  10,
+        'reverse_top_level' =>  false
+      ), $comments );
+    ?>
+  </ol>
+</div>
+
+
+<?php endwhile; ?>
+
+<?php get_footer(); ?> 
