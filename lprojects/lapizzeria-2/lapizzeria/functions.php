3e09ef0e05a1a71780029faeb0926fb705311dab
viseth89 <viseth89@gmail.com>
2019-04-03 12:06:33 -0500

    added scss to project and created menu

--- lprojects/lapizzeria-2/lapizzeria/functions.php
+++ lprojects/lapizzeria-2/lapizzeria/functions.php
@@ -7,11 +7,6 @@
   1.  1st paramater = Name of Stylesheet, 2nd Paramater = Path of Stylesheet, 3rd parameter pass in dependencies (optional), 4th Version #
   2.  Normalize CSS = Display elements universally amongst browsers
   3.  Here we pass normalize into our style.css as a dependency 'array('normalize')
-
-
-
-
-
   */
   wp_register_style('normalize', get_template_directory_uri() . 'css/normalize.css', array(), '1.0' );
   wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0' );
@@ -22,5 +17,25 @@
 
  }
 
-  // Hook the style, 1st parameter = , 2nd parameter = function 
+  // Hook the style, 1st Parameter = , 2nd Parameter = function 
  add_action('wp_enqueue_scripts', 'lapizzeria_styles');
+
+
+/*  Add Menus
+1.  Allows custom menu's to be created
+2.  Will show Menu's in "Appearance" in wp Admin Panel
+
+
+*/
+
+function lapizzeria_menus() {
+ register_nav_menus(array(
+  /*
+  1st Parameter = Name given to Menu : 2nd Parameter = Readable Name with '__' outside parenthesis, 3rd Parameter (optional) = domain name
+  */
+  'header-menu' => __('Header Menu', 'lapizzeria')
+ ) );
+  
+}
+
+add_action('init', 'lapizzeria_menus');
--- lprojects/lapizzeria-2/lapizzeria/header.php
+++ lprojects/lapizzeria-2/lapizzeria/header.php
@@ -1,10 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>La Pizzeria</title>
-  <?php wp_head() ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>La Pizzeria</title>
+    <?php wp_head() ?>
 </head>
+
 <body>
+
+    <header class="site-header">
+        <div class="logo">
+            <a href="<?php echo esc_url( home_url('/') ); ?>">
+                <img src="<?php echo get_template_directory_uri() ?>/img/logo.svg" alt="">
+            </a>
+        </div>
+    </header> 
+
+    <!-- WP Menu -->
+    <div class="main-menu">
+      <div class="navigation">
+        <?php 
+          $args = array(
+            'theme_location'  =>  'header-menu',
+            'container'       =>  'nav',
+            'container_class' =>  'site-nav'
+          );
+          // Use function 'wp_nav_menu()' and pass in $args/array
+          wp_nav_menu($args)
+        ?>
+      </div>
+    </div>
